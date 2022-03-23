70fb4eb6e73c645ae3250180db4905e51ff572c8
GerardoFesta <g.festa22@studenti.unisa.it>
2022-03-23 11:23:29 +0100

    inversione ordine script

--- src/r_scripts_loader.py
+++ src/r_scripts_loader.py
@@ -1,5 +1,6 @@
 import rpy2.robjects as robjects
 
 def runScripts():
-    robjects.r.source("SpotySuggestions/src/r/regressione.r", encoding="utf-8")
     robjects.r.source("SpotySuggestions/src/r/classificazione.r", encoding="utf-8")
+    
+    robjects.r.source("SpotySuggestions/src/r/regressione.r", encoding="utf-8")
