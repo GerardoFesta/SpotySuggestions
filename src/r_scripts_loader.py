8180e434629971ada52a1ab8e2b2d0e394a85376
GerardoFesta <g.festa22@studenti.unisa.it>
2022-03-26 18:19:49 +0100

    Cambio path in r_script_loader

--- src/r_scripts_loader.py
+++ src/r_scripts_loader.py
@@ -1,6 +1,4 @@
 import rpy2.robjects as robjects
 
 def runScripts():
-    robjects.r.source("SpotySuggestions/src/r/classificazione.r", encoding="utf-8")
-    
-    robjects.r.source("SpotySuggestions/src/r/regressione.r", encoding="utf-8")
+    robjects.r.source("SpotySuggestions/src/r/Finale.r")
